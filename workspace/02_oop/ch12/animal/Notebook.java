20a3854327f84d3649f009093053e192ab6511ff
Hye Ran Kim <mini10047@naver.com>
2026-06-12 11:36:28 +0900

    0612

--- workspace/02_oop/ch12/animal/Notebook.java
+++
@@ -1,7 +0,0 @@
-package ch12.animal;
-
-public class Notebook extends Animal {
-    void turnon(){
-        System.out.println("전원을 켠다.");
-    }
-}
--- workspace/02_oop/ch13/supercar/Chargeable.java
+++
@@ -1,5 +0,0 @@
-package ch13.supercar;
-
-public interface Chargeable {
-    void charge();
-}
